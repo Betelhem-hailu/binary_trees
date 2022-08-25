b2c55aeea5017905507f2e39b999e4f94092c5cf
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-08-25 10:49:02 +0300

    insert left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node- returns pointer to a new node or null 
+ * @parent: binary_tree_t
+ * @value: integer
+ *
+ * Return: pointer of new node on sucess
+ *         Otherwise - null on failure
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new;
+    new = malloc(sizeof(binary_tree_t));
+
+    if (new == NULL)
+        return (NULL);
+    
+    if (parent->left == NULL)
+    {
+    new->n = value;
+    new->parent = parent;
+    new->right = NULL;
+    new->left = NULL;
+    return (new);
+    }
+
+    new->n = value;
+    new->left = parent->left;
+    new->right = NULL;
+    new->parent = parent;
+    return (new);
+}

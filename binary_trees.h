0a8583368061c2d70a21e8d3deeac818e8293cb2
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-08-25 13:43:59 +0300

    leaf

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,9 @@
+#ifndef __BINARY_TREES_H__
+#define __BINARY_TREES_H__
+
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -30,3 +32,18 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+#endif /* __BINARY_TREES_H__ */

52a71604469d4bf8437cdd6ea0083f8e990735d4
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-08-25 14:04:42 +0300

    insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,7 +21,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
         {
                 new->left = parent->left;
                 parent->left->parent = new;
-                new->parent = parent;
         }
         parent->left = new;
 

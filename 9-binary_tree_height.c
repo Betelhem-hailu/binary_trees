f4da569a79390c6b9d408db19642f71c1cc888f0
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-08-25 17:19:02 +0300

    Height of tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,15 +9,19 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+    size_t left_size = 0;
+    size_t right_size = 0;
+
     if (tree == NULL)
         return (0);
 
-    int height;
-    while (tree->left != NULL)
-    {
-        tree->left = tree->left->left;
-        height++;
-    }
+    if (tree->left != NULL)
+        left_size += 1 + binary_tree_height(tree->left);
+    if (tree->right != NULL)
+        right_size += 1 + binary_tree_height(tree->right);
 
-    return (height);
+    if (left_size > right_size)
+        return (left_size);
+    else
+        return (right_size);
 }

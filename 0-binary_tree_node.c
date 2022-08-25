56b2a45ff25d5768be4a982193ba8eee168a348f
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-08-25 11:21:15 +0300

    new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,7 +14,7 @@
  {
     binary_tree_t *new;
 
-    new = malloc(sizeof(binary_tree_t));
+    new = (binary_tree_t*) malloc(sizeof(binary_tree_t));
     if(new == NULL)
             return (NULL);
     

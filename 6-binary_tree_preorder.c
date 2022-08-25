e4d74aadf5a18a1ae667121a348ae46225836e00
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-08-25 16:23:12 +0300

    preorder traverse

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -4,17 +4,16 @@
  * binary_tree_preorder: traverses in preorder
  * @tree:is pointer to root node
  * @func:is pointer to function to call each node
- * 
+ *
  * return: nothing
  */
 
-void binary_tree_preorder(const binary_tree_t *tree, void(*func)(int))
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-   if( tree == NULL || func == NULL )
-    return;
+    if (tree == NULL || func == NULL)
+        return;
 
     func(tree->n);
     binary_tree_preorder(tree->left, func);
     binary_tree_preorder(tree->right, func);
-
 }
